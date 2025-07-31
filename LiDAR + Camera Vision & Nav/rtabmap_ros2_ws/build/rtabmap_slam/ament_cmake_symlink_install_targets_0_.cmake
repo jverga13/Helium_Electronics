c925cf738faf2b6ab194c2cfc9b3ae75f9628c4c
JOHN VERGA <jverga@umich.edu>
2025-07-31 16:40:36 -0400

    shorter names

---
+++ LiDAR + Camera Vision & Nav/rtabmap_ros2_ws/build/rtabmap_slam/ament_cmake_symlink_install_targets_0_.cmake
@@ -0,0 +1,1 @@
+ament_cmake_symlink_install_targets("TARGET_FILES" "/home/jverga/rtabmap_ros2_ws/build/rtabmap_slam/rtabmap" "TARGETS" "rtabmap_node" "DESTINATION" "lib/rtabmap_slam")
---
+++ LiDAR + Camera Vision & Nav/rtabmap_ros2_ws/build/rtabmap_slam/colcon_command_prefix_build.sh
@@ -0,0 +1,5 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/jverga/rtabmap_ros2_ws/install/rtabmap_msgs/share/rtabmap_msgs/package.sh"
+. "/home/jverga/rtabmap_ros2_ws/install/rtabmap_conversions/share/rtabmap_conversions/package.sh"
+. "/home/jverga/rtabmap_ros2_ws/install/rtabmap_sync/share/rtabmap_sync/package.sh"
+. "/home/jverga/rtabmap_ros2_ws/install/rtabmap_util/share/rtabmap_util/package.sh"
